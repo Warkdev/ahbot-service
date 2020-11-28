e196195d89e536994d3cf3621db62e56434f5d0c
Warkdev <cedric.servais@outlook.com>
2020-11-28 22:49:41 +0100

    Add stuff in data.sql

---
+++ scripts/database/data.sql
@@ -0,0 +1,6 @@
+LOCK TABLES `ahbot_category` WRITE;
+/*!40000 ALTER TABLE `ahbot_category` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ahbot_category` ENABLE KEYS */;
+LOCK TABLES `ahbot_history` WRITE;
+LOCK TABLES `ahbot_price` WRITE;
+UNLOCK TABLES;
